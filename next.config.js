12054c6dedaa102be88a212d43b815894a7efda3
Bilal Tosun <40119366+bilaltosungit@users.noreply.github.com>
2025-01-04 22:55:58 +0300

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
   },
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
